7cf45852e70f0cb049dd8fc7dcda36a765d6f52a
landra5 <landra5@bancodebogota.com.co>
2021-02-03 11:27:43 -0500

    update: se deja repositorio como front

--- database/4-caida_servicios.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS operations;
-CREATE TABLE operations (
-    Id_operation        INT PRIMARY KEY auto_increment,
-    Name                VARCHAR(40) NOT NULL,
-    Initials            VARCHAR(10) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
